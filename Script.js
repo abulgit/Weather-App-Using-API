de6e9d5eae577dfd7695c6af2f3c14ab9da21c46
Abul Hossain Khan <abulkhan19175@gmail.com>
2023-09-15 21:43:10 +0530

    Testing done

--- Script.js
+++ Script.js
@@ -1,49 +1,73 @@
 const weatherApi = {
-    key: "9399278bfd50b085a715eba7e71402d3",
-    baseUrl: "https://api.openweathermap.org/data/2.5/weather"//API
-}
-const searchInputBox = document.getElementById('input-box');
-searchInputBox.addEventListener('keypress', (event) => {
-    if(event.keyCode == 13){
-        console.log(searchInputBox.value);
-        getWeatherReport(searchInputBox.value);
-        document.querySelector('.weather_body').style.display = "block";
-    }
+  key: "9399278bfd50b085a715eba7e71402d3",
+  baseUrl: "https://api.openweathermap.org/data/2.5/weather", //API
+};
+const searchInputBox = document.getElementById("input-box");
+searchInputBox.addEventListener("keypress", (event) => {
+  if (event.keyCode == 13) {
+    console.log(searchInputBox.value);
+    getWeatherReport(searchInputBox.value);
+    document.querySelector(".weather_body").style.display = "block";
+  }
 });
 function getWeatherReport(city) {
-    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
-    .then(weather => {
-        return weather.json();
-    }).then(showWeatherReport);
+  fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(showWeatherReport);
 }
 function showWeatherReport(weather) {
-    console.log(weather);
+  console.log(weather);
 
-    let city_name = document.getElementById('city');
-    city_name.innerText = `${weather.name} , ${weather.sys.country}`;
+  let city_name = document.getElementById("city");
+  city_name.innerText = `${weather.name} , ${weather.sys.country}`;
 
-    let temp = document.getElementById('temparature');
-    temp.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+  let temp = document.getElementById("temparature");
+  temp.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
 
-    let min_max_temp = document.getElementById('min-max');
-    min_max_temp.innerHTML = `${Math.round(weather.main.temp_max)}&deg;C(max) / ${Math.round(weather.main.temp_max)}&deg;C(min)`;
+  let min_max_temp = document.getElementById("min-max");
+  min_max_temp.innerHTML = `${Math.round(
+    weather.main.temp_max
+  )}&deg;C(max) / ${Math.round(weather.main.temp_max)}&deg;C(min)`;
 
-    let weather_type = document.getElementById('isCloud');
-    weather_type.innerHTML = `${weather.weather[0].main}`;
+  let weather_type = document.getElementById("isCloud");
+  weather_type.innerHTML = `${weather.weather[0].main}`;
 
-    let date = document.getElementById('date');
-    let todayDate = new Date();
-    date.innerText = dateManage(todayDate)
+  let date = document.getElementById("date");
+  let todayDate = new Date();
+  date.innerText = dateManage(todayDate);
 }
 function dateManage(datearg) {
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-    let year = datearg.getFullYear();
-    let month = months[datearg.getMonth()];
-    let date = datearg.getDate();
-    let day = days[datearg.getDay()];
+  let year = datearg.getFullYear();
+  let month = months[datearg.getMonth()];
+  let date = datearg.getDate();
+  let day = days[datearg.getDay()];
 
-    return `${day} , ${date} ${month} , ${year}`;
+  return `${day} , ${date} ${month} , ${year}`;
 }
